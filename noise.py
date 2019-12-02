9fa594a51fa66ef085b3f4d9bca1632793b7eca7
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2019-12-02 16:52:35 +0200

    Add base class

--- noise.py
+++ noise.py
@@ -1,14 +1,26 @@
 import numpy.random
 
 
-class WhiteGaussianNoise:
+class Noise:
+    """Base class for noise generators"""
+    def __call__(self):
+        raise NotImplementedError
+
+    def covariance(self):
+        raise NotImplementedError
+
+
+class WhiteGaussianNoise(Noise):
     """Contains information on the random variable used to generate white gaussian noise
     """
     def __init__(self, covariance):
-        self.covariance = covariance
-        N = self.covariance.shape[0]
+        self._covariance = covariance
+        N = self._covariance.shape[0]
         self.mean = numpy.zeros(N)
 
     def __call__(self):
-        return numpy.random.multivariate_normal(self.mean, self.covariance)
+        return numpy.random.multivariate_normal(self.mean, self._covariance)
+
+    def covariance(self):
+        return self._covariance
 
