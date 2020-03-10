af7af1f000172e4304d2a40306f852bd6befeee4
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-03-10 15:43:25 +0100

    Add predict()

--- filter/particle.py
+++ filter/particle.py
@@ -1,5 +1,5 @@
 import numpy
-import numba
+from numba import cuda
 
 
 class ParticleFilter:
@@ -11,9 +11,25 @@ class ParticleFilter:
         self.g = g
         self.N_particles = N_particles
 
-        self.particles = x0.draw(N_particles)
+        particles_host = x0.draw(N_particles)
+        self.particles = cuda.to_device(particles_host)
         self.weights = numpy.full(N_particles, 1/N_particles)
 
-    @numba.cuda.jit
     def predict(self, dt, u):
+        threads_per_block = 1024
+        blocks_per_grid = (self.N_particles - 1) // threads_per_block + 1
 
+        ParticleFilter.predict_kernel[blocks_per_grid, threads_per_block](dt, u, self.f, self.particles)
+
+    @staticmethod
+    @cuda.jit
+    def predict_kernel(dt, u, f, particles):
+        tx = cuda.threadIdx.x
+        bx = cuda.blockIdx.x
+        bw = cuda.blockDim.x
+        indx = bw * bx + tx
+
+        if indx >= particles.size:
+            return
+
+        particles[indx] = f(particles[indx], u, dt)
