a35f411c0109fd0490fe5b9e3c580c264dccc67e
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-03-11 06:40:51 +0100

    Add update()

--- filter/particle.py
+++ filter/particle.py
@@ -6,7 +6,7 @@ class ParticleFilter:
     """Implements a parallel particle filter algorithm.
     """
 
-    def __init__(self, f, g, N_particles, x0):
+    def __init__(self, f, g, N_particles, x0, measurment_pdf):
         self.f = f
         self.g = g
         self.N_particles = N_particles
@@ -14,12 +14,13 @@ class ParticleFilter:
         particles_host = x0.draw(N_particles)
         self.particles = cuda.to_device(particles_host)
         self.weights = numpy.full(N_particles, 1/N_particles)
+        self.measurment_pdf = measurment_pdf
 
-    def predict(self, dt, u):
-        threads_per_block = 1024
-        blocks_per_grid = (self.N_particles - 1) // threads_per_block + 1
+        self.threads_per_block = self.tpb = 1024
+        self.blocks_per_grid = self.bpg = (self.N_particles - 1) // self.threads_per_block + 1
 
-        ParticleFilter.predict_kernel[blocks_per_grid, threads_per_block](dt, u, self.f, self.particles)
+    def predict(self, u, dt):
+        ParticleFilter.predict_kernel[self.tpb, self.bpg](dt, u, self.f, self.particles)
 
     @staticmethod
     @cuda.jit
@@ -33,3 +34,22 @@ class ParticleFilter:
             return
 
         particles[indx] = f(particles[indx], u, dt)
+
+    def update(self, u, z):
+        ParticleFilter.update_kernel[self.tpb, self.bpg](u, z,
+                                                         self.g, self.particles, self.weights, self.measurment_pdf)
+
+    @staticmethod
+    @cuda.jit
+    def update_kernel(u, z, g, particles, weights, measurment_pdf):
+        tx = cuda.threadIdx.x
+        bx = cuda.blockIdx.x
+        bw = cuda.blockDim.x
+        indx = bw * bx + tx
+
+        if indx >= particles.size:
+            return
+
+        y_i = g(particles[indx], u)
+        e_i = z - y_i
+        weights[indx] *= measurment_pdf(e_i)
