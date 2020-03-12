4e33a5b0e25f3b8ec2b257cf30682dafa8fc1619
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-03-12 12:05:18 +0100

    Add initial files

---
+++ gpu_funcs/MultivariateGaussianSum.py
@@ -0,0 +1,24 @@
+class MultivariateGaussianSum:
+    """Allows efficient pdf lookup of multivarite Gaussian sum pdf for GPU code"""
+
+    def __init__(self, means, covariances, weights):
+        self.means_device = cuda.to_device(means)
+        self.weights_device = cuda.to_device(weights)
+
+        self.inverse_covariances_device = cuda.to_device(numpy.linalg.inv(covariances))
+        k = means.shape[1]
+
+        constants = (2*numpy.pi)**(-k/2) / numpy.sqrt(numpy.linalg.det(covariances))
+        self.constants_device = cuda.to_device(constants)
+
+    @cuda.jit(device=True)
+    def pdf(self, x):
+        es = x - self.means_device
+        result = 0
+        for i in range(self.means_device.shape[1]):
+            exp = es[i].T @ self.inverse_covariances_device[i] @ es[i]
+            r = numpy.exp(exp)
+            result += self.weights_device[i] * self.constants_device[i] * r
+
+
+        return result
---
+++ gpu_funcs/nicely_sample.py
@@ -0,0 +1,44 @@
+import numpy
+from numba import cuda
+import cupy
+
+print("In nicely_sampling.py: rewriting cupy.cumsum to be numpy.cumsum")
+cupy.cumsum = numpy.cumsum
+
+
+def nicely_systematic_sample(N, weights):
+    c_outer = cupy.cumsum(weights)
+
+    sample_indx_outer = numpy.zeros(N)
+    r = numpy.random.rand()
+    threads_per_block = 1024
+    blocks_per_grid = (N - 1) // threads_per_block + 1
+    N_weights = weights.size
+
+    @cuda.jit
+    def do_in_parallel(c, sample_indx):
+        tx = cuda.threadIdx.x
+        bx = cuda.blockIdx.x
+        bw = cuda.blockDim.x
+        i = bw * bx + tx
+
+        if i >= N:
+            return
+
+        u = (i + r) / N
+        k = int(i/N*N_weights)
+        while c[k] < u:
+            k += 1
+
+        cuda.syncthreads()
+
+        while c[k] > u and k >= 0:
+            k -= 1
+
+        cuda.syncthreads()
+
+        sample_indx[i] = k + 1
+
+    do_in_parallel[threads_per_block, blocks_per_grid](c_outer, sample_indx_outer)
+
+    return sample_indx_outer
