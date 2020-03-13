f761dd4f7048613480ee1ce771d7fbbe3006be0e
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-03-13 11:57:13 +0100

    Use cupy arrays instead of numpy ones

--- filter/particle.py
+++ filter/particle.py
@@ -1,26 +1,25 @@
-import numpy
 from numba import cuda
+import cupy
 
 
 class ParticleFilter:
     """Implements a parallel particle filter algorithm.
     """
 
-    def __init__(self, f, g, N_particles, x0, measurment_pdf):
+    def __init__(self, f, g, N_particles, x0, measurement_pdf):
         self.f = f
         self.g = g
         self.N_particles = N_particles
 
-        particles_host = x0.draw(N_particles)
-        self.particles = cuda.to_device(particles_host)
-        self.weights = numpy.full(N_particles, 1/N_particles)
-        self.measurment_pdf = measurment_pdf
+        self.particles_device = x0.draw(N_particles)  # Draws into GPU memory
+        self.weights_device = cupy.full(N_particles, 1 / N_particles)
+        self.measurement_pdf = measurement_pdf
 
         self.threads_per_block = self.tpb = 1024
         self.blocks_per_grid = self.bpg = (self.N_particles - 1) // self.threads_per_block + 1
 
     def predict(self, u, dt):
-        ParticleFilter.predict_kernel[self.tpb, self.bpg](dt, u, self.f, self.particles)
+        ParticleFilter.predict_kernel[self.tpb, self.bpg](dt, u, self.f, self.particles_device)
 
     @staticmethod
     @cuda.jit
@@ -37,11 +36,11 @@ class ParticleFilter:
 
     def update(self, u, z):
         ParticleFilter.update_kernel[self.tpb, self.bpg](u, z,
-                                                         self.g, self.particles, self.weights, self.measurment_pdf)
+                                                         self.g, self.particles_device, self.weights_device, self.measurement_pdf)
 
     @staticmethod
     @cuda.jit
-    def update_kernel(u, z, g, particles, weights, measurment_pdf):
+    def update_kernel(u, z, g, particles, weights, measurement_pdf):
         tx = cuda.threadIdx.x
         bx = cuda.blockIdx.x
         bw = cuda.blockDim.x
@@ -52,4 +51,4 @@ class ParticleFilter:
 
         y_i = g(particles[indx], u)
         e_i = z - y_i
-        weights[indx] *= measurment_pdf(e_i)
+        weights[indx] *= measurement_pdf(e_i)
