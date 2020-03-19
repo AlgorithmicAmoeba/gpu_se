1600c50dece95db9c2ced856a76ff2f9b54022f9
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-03-19 18:08:51 +0200

    Fix initialisation

--- gpu_funcs/MultivariateGaussianSum.py
+++ gpu_funcs/MultivariateGaussianSum.py
@@ -1,19 +1,20 @@
 import numpy
 import numba.cuda as cuda
+import cupy
 
 
 class MultivariateGaussianSum:
     """Allows efficient pdf lookup of multivarite Gaussian sum pdf for GPU code"""
 
     def __init__(self, means, covariances, weights):
-        self.means_device = cuda.to_device(means)
-        self.weights_device = cuda.to_device(weights)
+        self.means_device = cupy.asarray(means)
+        self.weights_device = cupy.asarray(weights)
+        self.covariances_device = cupy.asarray(covariances)
 
-        self.inverse_covariances_device = cuda.to_device(numpy.linalg.inv(covariances))
-        k = means.shape[1]
+        self.inverse_covariances_device = cupy.asarray(numpy.linalg.inv(covariances))
+        self.N, self.k = means.shape
 
-        constants = (2*numpy.pi)**(-k/2) / numpy.sqrt(numpy.linalg.det(covariances))
-        self.constants_device = cuda.to_device(constants)
+        self.constants_device = (2*cupy.pi)**(-self.k/2) / cupy.sqrt(cupy.linalg.det(self.covariances_device))
 
     @cuda.jit(device=True)
     def pdf(self, x):
