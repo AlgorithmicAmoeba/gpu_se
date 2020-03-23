36a251ba76b69e522b5242b5c0c02a3f290b1f46
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-03-23 10:35:04 +0200

    Make object cpu/gpu agnostic

--- gpu_funcs/MultivariateGaussianSum.py
+++ gpu_funcs/MultivariateGaussianSum.py
@@ -4,41 +4,44 @@ import cupy
 
 
 class MultivariateGaussianSum:
-    """Allows efficient pdf lookup of multivarite Gaussian sum pdf for GPU code"""
+    """Allows efficient pdf lookup of multivarite Gaussian sum pdf for CPU or GPU code"""
 
-    def __init__(self, means, covariances, weights):
-        self.means_device = cupy.asarray(means)
-        self.weights_device = cupy.asarray(weights)
-        self.covariances_device = cupy.asarray(covariances)
+    def __init__(self, means, covariances, weights, library=cupy):
+        self.lib = library
+        self.means_device = self.lib.asarray(means)
+        self.weights_device = self.lib.asarray(weights)
+        self.covariances_device = self.lib.asarray(covariances)
 
-        self.inverse_covariances_device = cupy.asarray(numpy.linalg.inv(covariances))
+        self.inverse_covariances_device = self.lib.asarray(numpy.linalg.inv(covariances))
         self.N, self.k = means.shape
 
-        self.constants_device = (2*cupy.pi)**(-self.k/2) / cupy.sqrt(cupy.linalg.det(self.covariances_device))
+        self.constants_device = (2 * self.lib.pi) ** (-self.k / 2) / self.lib.sqrt(
+            self.lib.linalg.det(self.covariances_device))
 
     def pdf(self, x):
-        x = cupy.asarray(x)
-        
         es = x - self.means_device
-        # exp[i] = es[i].T @ self.inverse_covariances_device[i] @ es[i]
-        exp = cupy.einsum('abc, cd, ed -> a', self.inverse_covariances_device, es, es)
-        r = cupy.exp(-exp)
-        # result = sum(r[i] * self.weights_device[i] * self.constants_device[i])
-        result = cupy.einsum('i, i, i -> ', r, self.weights_device, self.constants_device)
+
+        # The code below does: exp[i] = es[i].T @ self.inverse_covariances_device[i] @ es[i]
+        exp = self.lib.einsum('abc, cd, ed -> a', self.inverse_covariances_device, es, es)
+        r = self.lib.exp(-exp)
+
+        # The code below does: result = sum(r[i] * self.weights_device[i] * self.constants_device[i])
+        result = self.lib.einsum('i, i, i -> ', r, self.weights_device, self.constants_device)
 
         return result
 
     def draw(self, shape=(1,)):
         if isinstance(shape, int):
-            shape = (shape, )
+            shape = (shape,)
 
         size = int(numpy.prod(shape))
-        bins = cupy.bincount(cupy.random.choice(cupy.arange(self.N), size, p=self.weights_device), minlength=self.N)
-        out = cupy.empty((size, self.k))
+        bins = self.lib.bincount(self.lib.random.choice(self.lib.arange(self.N), size, p=self.weights_device),
+                                 minlength=self.N)
+        out = self.lib.empty((size, self.k))
 
         index = 0
         for n, mean, cov in zip(bins, self.means_device, self.covariances_device):
-            out[index:index+n] = cupy.random.multivariate_normal(mean, cov, int(n))
+            out[index:index + n] = self.lib.random.multivariate_normal(mean, cov, int(n))
             index += n
 
         return out.reshape(shape + (self.k, ))
