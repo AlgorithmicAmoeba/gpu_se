c3c1037c0c641236c847ec1cbcf67b6797cc07bf
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-04-01 12:05:01 +0200

    Add memoisation

--- results/PF_update.py
+++ results/PF_update.py
@@ -8,16 +8,32 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-def generate_results():
-    N = 15
+def generate_results(redo=False):
+    try:
+        if redo:
+            raise FileNotFoundError
+
+        df = pandas.read_csv('PF_update.csv', index_col=0)
+    except FileNotFoundError:
+        df = pandas.DataFrame(columns=['CPU', 'GPU'])
+
+    N_done = df.shape[0]
+    N = 25
+
+    if N_done >= N:
+        return
+
     count = 10
-    times = numpy.zeros((N, 2))
+    times = numpy.zeros((N - N_done, 2))
 
     z = numpy.array([2.3, 1.2])
 
-    for i in tqdm.tqdm(range(N)):
-        p = ParticleFilter(f, g, 2**(i+1), x0_cpu, measurement_noise_cpu)
-        pp = ParallelParticleFilter(f, g, 2**(i+1), x0_gpu, measurement_noise_gpu)
+    for i in tqdm.tqdm(range(N - N_done)):
+        if i < N_done:
+            continue
+
+        p = ParticleFilter(f, g, 2**(N_done + i+1), x0_cpu, measurement_noise_cpu)
+        pp = ParallelParticleFilter(f, g, 2**(N_done + i+1), x0_gpu, measurement_noise_gpu)
 
         t_cpu = time.time()
         for j in range(count):
@@ -29,12 +45,13 @@ def generate_results():
             pp.update(1.0, z)
         times[i, 1] = time.time() - t_gpu
 
-    df = pandas.DataFrame(times, columns=['CPU', 'GPU'], index=range(1, N+1))
+    df_new = pandas.DataFrame(times, columns=['CPU', 'GPU'], index=range(N_done + 1, N + 1))
+    df = df.append(df_new)
     df.to_csv('PF_update.csv')
 
 
 def plot_results():
-    df = pandas.read_csv('PF_update.csv')
+    df = pandas.read_csv('PF_update.csv', index_col=0)
     df['speedup'] = df['CPU']/df['GPU']
     plt.semilogy(df.index, df['speedup'], '.')
 
@@ -42,7 +59,10 @@ def plot_results():
     plt.ylabel('Speed-up')
     plt.xlabel('$ \log_2(N) $ particles')
 
+    plt.savefig('PF_update.pdf')
+    plt.show()
+
 
 if __name__ == '__main__':
-    generate_results()
+    generate_results(True)
     plot_results()
