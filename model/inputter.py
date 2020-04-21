0b3559d91f293c1d2f1f9bc72fc1b71704b94b8b
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-04-21 07:52:47 +0200

    Change values

--- model/inputter.py
+++ model/inputter.py
@@ -9,7 +9,7 @@ class Inputs:
 
         Cn_in = 0.625 * 10 / 60  # (g/L) / (g/mol) = mol/L
         if t < t_batch + 66:
-            CgFg = 0.141612826257827
+            CgFg = 0.151612826257827
         elif t < t_batch + 101:
             CgFg = 0.21241923938674
         elif t < t_batch + 137:
@@ -18,7 +18,7 @@ class Inputs:
             CgFg = 0.354032065644566
 
         # if t < t_batch + 66:
-        #     CgFg = 0.141612826257827
+        #     CgFg = 0.151612826257827
         # else:
         #     CgFg = 0.21241923938674
         Cg_in = 314.19206 / 180  # (g/L) / (g/mol) = mol/L
@@ -35,7 +35,7 @@ class Inputs:
             Fg_in = CgFg / 180 / Cg_in  # (g/h) / (g/mol) / (mol/L) = L/h
             Fn_in = 0.625 / 1000 / Cn_in / 60  # (mg/h) / (mg/g) / (mol/L) / (g/mol) = L/h
             Fa_in = 0
-            Fb_in = 0.00006  # L/h
+            Fb_in = 6e-9  # L/h
             F_out = Fg_in + Fn_in + Fb_in + Fm_in
 
         T_amb = 25
