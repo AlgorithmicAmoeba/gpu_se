297cb57f16be8b157ac1e72dcdb8e741eef061d7
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-04-30 13:12:12 +0200

    Add docstrings

--- model/NonlinearModel.py
+++ model/NonlinearModel.py
@@ -1,10 +1,29 @@
 class NonlinearModel:
     """Base class for nonlinear models"""
     def DEs(self, inputs):
+        """Contains the differential and algebraic equations for the system model.
+
+        Parameters
+        ----------
+        inputs : array-like
+        """
         raise NotImplementedError
 
     def step(self, dt, inputs):
+        """Updates the model with inputs
+
+        Parameters
+        ----------
+        dt : float
+        inputs : array-like
+        """
         raise NotImplementedError
 
     def outputs(self, inputs):
+        """Returns all the outputs (state and calculated)
+
+        Parameters
+        ----------
+        inputs : array-like
+        """
         raise NotImplementedError
