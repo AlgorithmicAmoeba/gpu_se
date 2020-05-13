b5f619fe021aa4f5ba2b7615272de63039d0eaae
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-05-13 09:16:18 +0200

    Add DiagTank model

---
+++ model/DiagTank.py
@@ -0,0 +1,61 @@
+import numpy
+import model.NonlinearModel
+import model.TankModel
+
+
+class DiagTank(model.NonlinearModel):
+    def __init__(self, X0, t0=0):
+        self.X = numpy.array(X0)
+        self.t = t0
+
+        self.linear_tank = model.TankModel(X0[0], linear=True)
+        self.nonlinear_tank = model.TankModel(X0[1], linear=False)
+
+    def DEs(self, inputs):
+        """Contains the differential and algebraic equations for the system model.
+
+        Parameters
+        ----------
+        inputs : ndarray
+            The inputs to the system at the current time
+
+        Returns
+        -------
+        dX : array_like
+            The differential changes to the state variables
+        """
+
+        dh_lin = self.linear_tank.DEs(inputs[0])
+        dh_nonlin = self.linear_tank.DEs(inputs[1])
+
+        dX = numpy.hstack([dh_lin, dh_nonlin])
+
+        return dX
+
+    def step(self, dt, inputs):
+        """Updates the model with inputs
+
+        Parameters
+        ----------
+        inputs
+        dt : float
+            Time since previous step
+
+        """
+        self.t += dt
+        dX = self.DEs(inputs)
+        self.X += dX*dt
+        return self.outputs(inputs)
+
+    def outputs(self, inputs):
+        """Returns all the outputs (state and calculated)
+
+        Returns
+        -------
+        outputs : array_like
+            List of all the outputs from the model
+        """
+
+        outs = numpy.array(self.X)
+        _ = inputs
+        return outs.copy()
