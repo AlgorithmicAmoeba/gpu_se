5e8e4c82a8aadfebd4d4dcfbd12ffac93b728a5b
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-05-18 07:10:30 +0200

    Update spacing

--- tests/MultivariateGaussianSum_test.py
+++ tests/MultivariateGaussianSum_test.py
@@ -2,14 +2,15 @@ import numpy
 import cupy
 from gpu_funcs.MultivariateGaussianSum import MultivariateGaussianSum
 
-m = MultivariateGaussianSum(means=numpy.array([[10, 0],
-                                               [-10, -10]]),
-                            covariances=numpy.array([[[1, 0],
-                                                      [0, 1]],
+m = MultivariateGaussianSum(
+    means=numpy.array([[10, 0],
+                       [-10, -10]]),
+    covariances=numpy.array([[[1, 0],
+                              [0, 1]],
 
-                                                     [[2, 0.5],
-                                                      [0.5, 0.5]]]),
-                            weights=numpy.array([0.3, 0.7]))
+                             [[2, 0.5],
+                              [0.5, 0.5]]]),
+    weights=numpy.array([0.3, 0.7]))
 
 x = cupy.array([-10, -10])
 pdf_test = m.pdf(x)
