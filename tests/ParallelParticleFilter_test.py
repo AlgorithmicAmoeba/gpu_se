f6b4fb216d54ba13de6bf7b45198b07e3799faf1
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-05-18 09:35:39 +0200

    Update ParallelParticleFilter_test with pytest functions

--- tests/ParallelParticleFilter_test.py
+++ tests/ParallelParticleFilter_test.py
@@ -15,17 +15,19 @@ def f(x, u, dt):
 def g(x, u):
     x1, x2 = x
 
-    return [x1 * x2, x2 + u]
+    return x1 * x2, x2 + u
 
 
-x0 = MultivariateGaussianSum(means=numpy.array([[10, 0],
-                                                [-10, -10]]),
-                             covariances=numpy.array([[[1, 0],
-                                                       [0, 1]],
+x0 = MultivariateGaussianSum(
+    means=numpy.array([[10, 0],
+                       [-10, -10]]),
+    covariances=numpy.array([[[1, 0],
+                              [0, 1]],
 
-                                                      [[2, 0.5],
-                                                       [0.5, 0.5]]]),
-                             weights=numpy.array([0.3, 0.7]))
+                             [[2, 0.5],
+                              [0.5, 0.5]]]),
+    weights=numpy.array([0.3, 0.7])
+)
 
 state_noise = MultivariateGaussianSum(
     means=numpy.array([[1e-3, 0],
@@ -35,7 +37,6 @@ state_noise = MultivariateGaussianSum(
                              [[2e-4, 1e-5],
                               [1e-5, 5e-6]]]),
     weights=numpy.array([0.5, 0.5]),
-    library=numpy
 )
 
 measurement_noise = MultivariateGaussianSum(
@@ -46,9 +47,19 @@ measurement_noise = MultivariateGaussianSum(
                              [[0.2, 0.01],
                               [0.01, 0.005]]]),
     weights=numpy.array([0.7, 0.9]),
-    library=numpy
 )
 
 pp = ParallelParticleFilter(f, g, 10, x0, state_noise, measurement_noise)
 
-pp.predict(1, 1)
+
+def test_ParallelParticleFilter_predict():
+    pp.predict(1, 1)
+
+
+def test_ParallelParticleFilter_update():
+    z = numpy.array([2.3, 1.2])
+    pp.update(1.0, z)
+
+
+def test_ParallelParticleFilter_resample():
+    pp.resample()
