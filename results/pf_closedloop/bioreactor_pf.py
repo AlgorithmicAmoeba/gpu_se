4dd45d5b1a8c04011bab64c594646cabea242f7c
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-05-19 12:38:09 +0200

    Add PF predictions

--- results/pf_closedloop/bioreactor_pf.py
+++ results/pf_closedloop/bioreactor_pf.py
@@ -1,7 +1,6 @@
 import numpy
 import tqdm
 import matplotlib.pyplot as plt
-import cupy
 import controller
 import model.LinearModel
 import gpu_funcs.MultivariateGaussianSum
@@ -61,15 +60,15 @@ K = controller.MPC(
 pf = filter.ParallelParticleFilter(
     f=bioreactor.homeostatic_DEs,
     g=bioreactor.static_outputs,
-    N_particles=2**15,
+    N_particles=2**20,
     x0=gpu_funcs.MultivariateGaussianSum(
         means=bioreactor.X[numpy.newaxis, :],
-        covariances=numpy.diag([1e-7, 1e-6, 1e-7, 1e-7, 1e-7])[numpy.newaxis, :, :],
+        covariances=numpy.diag([1e-10, 1e-8, 1e-9, 1e-9, 1e-9])[numpy.newaxis, :, :],
         weights=numpy.array([1.])
     ),
     state_pdf=gpu_funcs.MultivariateGaussianSum(
         means=numpy.zeros(shape=(1, 5)),
-        covariances=numpy.diag([1e-9, 1e-8, 1e-9, 1e-9, 1e-9])[numpy.newaxis, :, :]*1e-10,
+        covariances=numpy.diag([1e-10, 1e-8, 1e-9, 1e-9, 1e-9])[numpy.newaxis, :, :],
         weights=numpy.array([1.])
     ),
     measurement_pdf=gpu_funcs.MultivariateGaussianSum(
@@ -88,6 +87,12 @@ pf = filter.ParallelParticleFilter(
 us = [numpy.array([0.06, 5/180, 0.2])]
 ys = [bioreactor.outputs(us[-1])]
 xs = [bioreactor.X.copy()]
+ys_pf = [
+    model.Bioreactor.static_outputs(
+            (pf.weights_device @ pf.particles_device).get(),
+            us[-1]
+        )
+]
 
 biass = []
 
@@ -100,7 +105,7 @@ for t in tqdm.tqdm(ts[1:]):
 
         pf.update(us[-1], ys[-1][lin_model.outputs])
         pf.resample()
-        x_pf = cupy.mean(cupy.asarray(pf.particles_device), axis=1).get()
+        x_pf = (pf.weights_device @ pf.particles_device).get()
         u = K.step(lin_model.xn2d(x_pf), lin_model.un2d(us[-1]), lin_model.yn2d(ys[-1]))
         U_temp[lin_model.inputs] = lin_model.ud2n(u)
         us.append(U_temp.copy())
@@ -116,36 +121,48 @@ for t in tqdm.tqdm(ts[1:]):
     xs.append(bioreactor.X.copy())
 
     pf.predict(us[-1], dt)
+    ys_pf.append(
+        numpy.array(
+            model.Bioreactor.static_outputs(
+                (pf.weights_device @ pf.particles_device).get(),
+                us[-1]
+            )
+        )
+    )
 
 ys = numpy.array(ys)
 us = numpy.array(us)
 xs = numpy.array(xs)
+ys_pf = numpy.array(ys_pf)
 biass = numpy.array(biass)
 
 plt.subplot(2, 3, 1)
 plt.plot(ts, ys[:, 2])
+plt.plot(ts, ys_pf[:, 1])
 plt.title('Cfa')
 
 plt.subplot(2, 3, 2)
+plt.plot(ts, ys[:, 0])
+plt.plot(ts, ys_pf[:, 0])
+plt.title('Cg')
+
+plt.subplot(2, 3, 3)
+plt.plot(ts, ys[:, 3])
+plt.title('Ce')
+
+plt.subplot(2, 3, 4)
 plt.plot(ts, us[:, lin_model.inputs[0]])
 plt.title('Fm_in')
 
-plt.subplot(2, 3, 3)
-plt.plot(ts, ys[:, 0])
-plt.title('Cg')
+plt.subplot(2, 3, 5)
+plt.plot(ts, us[:, lin_model.inputs[1]])
+plt.title('Fg_in')
 
-plt.subplot(2, 3, 4)
+plt.subplot(2, 3, 6)
 plt.plot(
     numpy.arange(dt_control, end_time, dt_control),
     biass
 )
 plt.title('bias')
 
-plt.subplot(2, 3, 5)
-plt.plot(ts, ys[:, 3])
-plt.title('Ce')
-
-plt.subplot(2, 3, 6)
-plt.plot(ts, us[:, lin_model.inputs[1]])
-plt.title('Fg_in')
 plt.show()
