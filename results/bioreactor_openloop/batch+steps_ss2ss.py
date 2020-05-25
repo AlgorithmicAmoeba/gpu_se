cd35305a987bfe5ca9a2634e0f9a2f1a508235cd
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-05-25 16:28:54 +0200

    Clean up code

--- results/bioreactor_openloop/batch+steps_ss2ss.py
+++ results/bioreactor_openloop/batch+steps_ss2ss.py
@@ -5,7 +5,7 @@ import sim_base
 import model
 
 # Simulation set-up
-end_time = 800
+end_time = 500
 ts = numpy.linspace(0, end_time, end_time*10)
 dt = ts[1]
 
@@ -28,7 +28,6 @@ ys = [bioreactor.outputs(us[-1])]
 ys_meas = [bioreactor.outputs(us[-1])]
 
 not_cleared = True
-Cx = numpy.NaN
 for t in tqdm.tqdm(ts[1:]):
     if t < 25:
         us.append(numpy.array([0., 5/180, 0.]))
@@ -37,19 +36,10 @@ for t in tqdm.tqdm(ts[1:]):
             bioreactor.X[[0, 2, 3, 4]] = 0
             not_cleared = False
             bioreactor.high_N = False
-            Cx = bioreactor.X[1]
 
-        # (L/min) = (gG/gX/min) (molG/gG) (molX/Lv) (gX/molX) (Lv) (L/molG)
-        glucose = 0.3 / 180 * Cx * 24.6 * 1 / (5/180)
-        us.append(numpy.array([glucose, 5/180, 0.]))
+        us.append(numpy.array([0.06, 5/180, 0.2]))
     elif t < 500:
-        # (L/min) = (gG/gX/min) (molG/gG) (molX/Lv) (gX/molX) (Lv) (L/molG)
-        glucose = 0.45 / 180 * Cx * 24.6 * 1 / (5/180)
-        us.append(numpy.array([glucose, 5/180, 0.]))
-    elif t < 700:
-        # (L/min) = (gG/gX/min) (molG/gG) (molX/Lv) (gX/molX) (Lv) (L/molG)
-        glucose = 0.6 / 180 * Cx * 24.6 * 1 / (5/180)
-        us.append(numpy.array([glucose, 5/180, 0.]))
+        us.append(numpy.array([0.04, 5/180, 0.1]))
     else:
         us.append(us[-1])
 
@@ -68,7 +58,7 @@ xs = numpy.array(xs)
 
 
 def add_time_lines():
-    for time in [25, 200, 500, 700]:
+    for time in [25, 200]:
         plt.axvline(time, color='black', alpha=0.4)
     plt.xlim([0, ts[-1]])
 
@@ -97,16 +87,13 @@ plt.subplot(2, 3, 5)
 plt.plot(ts, us[:, select_inputs[0]])
 plt.title(r'$F_{G, in}$')
 plt.xlim([0, ts[-1]])
-for c in [0.4, 0.5]:
-    glucose = c / 180 * bioreactor.X[1] * 24.6 * 1 / (5/180)
-    plt.axhline(glucose, color='green', alpha=0.4)
 
 plt.subplot(2, 3, 6)
 plt.plot(ts, ys[:, 1])
 plt.title(r'$C_{X}$')
 add_time_lines()
 
-plt.suptitle('Openloop growth and production run')
+plt.suptitle('Openloop transition between steady states')
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-plt.savefig('batch+steps_noisy.pdf')
+plt.savefig('batch+steps_ss2ss.pdf')
 plt.show()
