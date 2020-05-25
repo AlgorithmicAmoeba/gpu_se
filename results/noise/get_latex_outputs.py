86213b4dbd2ab83951b663f099b174d400b0cb4b
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-05-25 16:29:35 +0200

    Output latex code for noise distributions

---
+++ results/noise/get_latex_outputs.py
@@ -0,0 +1,17 @@
+import sim_base
+import sympy
+import numpy
+
+state_pdf, measurement_pdf = sim_base.get_noise()
+
+sympy.print_latex(sympy.Matrix(numpy.diag(state_pdf.covariances_device[0])).T)
+
+sympy.print_latex(sympy.Matrix(measurement_pdf.means_device[0]).T)
+
+sympy.print_latex(sympy.Matrix(measurement_pdf.means_device[1]).T)
+
+sympy.print_latex(sympy.Matrix(numpy.diag(measurement_pdf.covariances_device[0])).T)
+
+sympy.print_latex(sympy.Matrix(numpy.diag(measurement_pdf.covariances_device[1])).T)
+
+sympy.print_latex(sympy.Matrix(measurement_pdf.weights_device).T)
