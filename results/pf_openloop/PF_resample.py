532f833b89c188f7a9136f0f85534abaa13d8bf0
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-06-03 08:05:56 +0200

    Get more resample data

--- results/pf_openloop/PF_resample.py
+++ results/pf_openloop/PF_resample.py
@@ -16,7 +16,7 @@ def generate_results(redo=False, cpu=True):
         df = pandas.DataFrame(columns=['CPU', 'GPU'])
 
     N_done = df.shape[0]
-    N = 20
+    N = 24
 
     if N_done >= N:
         return
@@ -67,5 +67,5 @@ def plot_results():
 
 
 if __name__ == '__main__':
-    generate_results(redo=True)
+    generate_results(redo=True, cpu=False)
     plot_results()
