056ab76afb5c5cceca70b767c8d91bbb850d93f7
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-06-10 09:06:53 +0200

    Update test

--- tests/ParallelParticleFilter_test.py
+++ tests/ParallelParticleFilter_test.py
@@ -1,57 +1,10 @@
 import numpy
-from filter.particle import ParallelParticleFilter
-from gpu_funcs.MultivariateGaussianSum import MultivariateGaussianSum
+import sim_base
 
-
-def f(x, u, dt):
-    x1, x2 = x
-    u, = u
-
-    dx1 = x1 - x2 * u
-    dx2 = x1 / u + 2 * x2
-
-    return x1 + dx1 * dt, x2 + dx2 * dt
-
-
-def g(x, u):
-    x1, x2 = x
-    u, = u
-
-    return x1 * x2, x2 + u
-
-
-x0 = MultivariateGaussianSum(
-    means=numpy.array([[10, 0],
-                       [-10, -10]]),
-    covariances=numpy.array([[[1, 0],
-                              [0, 1]],
-
-                             [[2, 0.5],
-                              [0.5, 0.5]]]),
-    weights=numpy.array([0.3, 0.7])
+_, _, _, pp = sim_base.get_parts(
+    N_particles=2**24,
+    gpu=True
 )
-
-state_noise = MultivariateGaussianSum(
-    means=numpy.array([[1e-3, 0],
-                       [0, -1e-3]]),
-    covariances=numpy.array([[[1e-4, 0],
-                              [0, 1e-5]],
-                             [[2e-4, 1e-5],
-                              [1e-5, 5e-6]]]),
-    weights=numpy.array([0.5, 0.5]),
-)
-
-measurement_noise = MultivariateGaussianSum(
-    means=numpy.array([[1, 0],
-                       [0, -1]]),
-    covariances=numpy.array([[[0.1, 0],
-                              [0, 0.1]],
-                             [[0.2, 0.01],
-                              [0.01, 0.005]]]),
-    weights=numpy.array([0.7, 0.3]),
-)
-
-pp = ParallelParticleFilter(f, g, 10, x0, state_noise, measurement_noise)
 
 
 def test_ParallelParticleFilter_predict():
