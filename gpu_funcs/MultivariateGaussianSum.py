47f537ffb3f68f3e1e615998e5f396791758457b
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-07-01 09:14:54 +0200

    Rename variables

--- gpu_funcs/MultivariateGaussianSum.py
+++ gpu_funcs/MultivariateGaussianSum.py
@@ -9,15 +9,15 @@ class MultivariateGaussianSum:
 
     def __init__(self, means, covariances, weights, library=cupy):
         self.lib = library
-        self.means_device = self.lib.asarray(means)
-        self.weights_device = self.lib.asarray(weights)
-        self.covariances_device = self.lib.asarray(covariances)
+        self.means = self.lib.asarray(means)
+        self.weights = self.lib.asarray(weights)
+        self.covariances = self.lib.asarray(covariances)
 
-        self.inverse_covariances_device = self.lib.asarray(numpy.linalg.inv(covariances))
+        self.inverse_covariances = self.lib.asarray(numpy.linalg.inv(covariances))
         self.Nd, self.Nx = means.shape
 
-        self.constants_device = (2 * self.lib.pi) ** (-self.Nx / 2) / self.lib.sqrt(
-            self.lib.linalg.det(self.covariances_device))
+        self.constants = (2 * self.lib.pi) ** (-self.Nx / 2) / self.lib.sqrt(
+            self.lib.linalg.det(self.covariances))
 
     def pdf(self, x):
         if len(x.shape) == 1:
@@ -25,15 +25,15 @@ class MultivariateGaussianSum:
         else:
             Np = x.shape[0]
         x = x.reshape((Np, 1, self.Nx))
-        means_device = self.means_device.reshape((1, self.Nd, self.Nx))
+        means_device = self.means.reshape((1, self.Nd, self.Nx))
         es = x - means_device
 
         # The code below does: exp[i] = es[i].T @ self.inverse_covariances_device[i] @ es[i]
-        exp = self.lib.einsum('...bc, ...c, ...b -> ...', self.inverse_covariances_device, es, es)
+        exp = self.lib.einsum('...bc, ...c, ...b -> ...', self.inverse_covariances, es, es)
         r = self.lib.exp(-exp)
 
         # The code below does: result = sum(r[i] * self.weights_device[i] * self.constants_device[i])
-        result = self.lib.einsum('...i, i, i -> ...', r, self.weights_device, self.constants_device)
+        result = self.lib.einsum('...i, i, i -> ...', r, self.weights, self.constants)
 
         return result
 
@@ -42,12 +42,12 @@ class MultivariateGaussianSum:
             shape = (shape,)
 
         size = int(numpy.prod(shape))
-        bins = self.lib.bincount(self.lib.random.choice(self.lib.arange(self.Nd), size, p=self.weights_device),
+        bins = self.lib.bincount(self.lib.random.choice(self.lib.arange(self.Nd), size, p=self.weights),
                                  minlength=self.Nd)
         out = self.lib.empty((size, self.Nx), dtype=self.lib.float32)
 
         index = 0
-        for n, mean, cov in zip(bins, self.means_device, self.covariances_device):
+        for n, mean, cov in zip(bins, self.means, self.covariances):
             out[index:index + n] = self.lib.random.multivariate_normal(mean, cov, int(n))
             index += n
 
