a6e14846d99a22f4090f97043443fbed7ca9e7d6
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-07-02 16:08:42 +0200

    Rename local vars

--- tests/GSUKF_test.py
+++ tests/GSUKF_test.py
@@ -5,40 +5,40 @@ import filter
 
 
 def test_gsufk():
-    state_pdfi, measurement_pdfi = sim_base.get_noise(lib=numpy)
+    state_pdf, measurement_pdf = sim_base.get_noise(lib=numpy)
     bioreactor, _, _, _ = sim_base.get_parts(gpu=False)
-    x0i, _ = sim_base.get_noise(lib=numpy)
-    x0i.means = bioreactor.X[numpy.newaxis, :]
+    x0, _ = sim_base.get_noise(lib=numpy)
+    x0.means = bioreactor.X[numpy.newaxis, :]
     gf = filter.GaussianSumUnscentedKalmanFilter(
         f=bioreactor.homeostatic_DEs,
         g=bioreactor.static_outputs,
         N_gaussians=7,
-        x0=x0i,
-        state_pdf=state_pdfi,
-        measurement_pdf=measurement_pdfi
+        x0=x0,
+        state_pdf=state_pdf,
+        measurement_pdf=measurement_pdf
     )
 
-    ui, zi = sim_base.get_random_io()
-    gf.predict(ui, 0.1)
-    gf.update(ui, zi)
+    u, z = sim_base.get_random_io()
+    gf.predict(u, 0.1)
+    gf.update(u, z)
     gf.resample()
 
 
 def test_pgfukf():
-    state_pdfi, measurement_pdfi = sim_base.get_noise(lib=cupy)
+    state_pdf, measurement_pdf = sim_base.get_noise(lib=cupy)
     bioreactor, _, _, _ = sim_base.get_parts(gpu=True)
-    x0i, _ = sim_base.get_noise(lib=cupy)
-    x0i.means = bioreactor.X[numpy.newaxis, :]
+    x0, _ = sim_base.get_noise(lib=cupy)
+    x0.means = bioreactor.X[numpy.newaxis, :]
     gf = filter.ParallelGaussianSumUnscentedKalmanFilter(
         f=bioreactor.homeostatic_DEs,
         g=bioreactor.static_outputs,
         N_gaussians=7,
-        x0=x0i,
-        state_pdf=state_pdfi,
-        measurement_pdf=measurement_pdfi
+        x0=x0,
+        state_pdf=state_pdf,
+        measurement_pdf=measurement_pdf
     )
 
-    ui, zi = sim_base.get_random_io()
-    gf.predict(ui, 0.1)
-    gf.update(ui, zi)
+    u, z = sim_base.get_random_io()
+    gf.predict(u, 0.1)
+    gf.update(u, z)
     gf.resample()
