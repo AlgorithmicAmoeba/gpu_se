c121e5683e848197da5433398b310055ea4844f1
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-07-03 09:41:41 +0200

    Make class a singleton

--- gpu_funcs/DeterministicGaussianSum.py
+++ gpu_funcs/DeterministicGaussianSum.py
@@ -4,12 +4,13 @@ import gpu_funcs.MultivariateGaussianSum
 
 
 class DeterministicGaussianSum(gpu_funcs.MultivariateGaussianSum):
-    """Creates a MultivariateGaussianSum object that always returns the same values.
+    """Creates a MultivariateGaussianSum singleton that always returns the same values.
     Useful for testing."""
-    def __init__(self,  means, covariances, weights, library=cupy):
-        super().__init__(means, covariances, weights, library=cupy)
 
-        self.values = library.array([])
+    values = numpy.array([], dtype=numpy.float32)
+
+    def __init__(self,  means, covariances, weights, library=cupy):
+        super().__init__(means, covariances, weights, library)
 
     def draw(self, shape=(1, )):
         if not isinstance(shape, tuple):
@@ -17,12 +18,19 @@ class DeterministicGaussianSum(gpu_funcs.MultivariateGaussianSum):
 
         size = int(numpy.prod(shape)) * self.Nx
 
-        if self.values.size < size:
-            drawn_vals = super().draw(size - self.values.size)
-            self.values = self.lib.hstack([
-                self.values,
+        if DeterministicGaussianSum.values.size < size:
+            drawn_vals = super().draw(size - DeterministicGaussianSum.values.size)
+
+            if self.lib == cupy:
+                drawn_vals = drawn_vals.get()
+
+            DeterministicGaussianSum.values = numpy.hstack([
+                DeterministicGaussianSum.values,
                 drawn_vals.flatten()
             ])
 
-        out = self.values[:size]
-        return out.reshape(shape + (self.Nx,)).squeeze()
+        out = DeterministicGaussianSum.values[:size]
+        out = out.reshape(shape + (self.Nx,)).squeeze()
+        if self.lib == cupy:
+            out = cupy.asarray(out)
+        return out
