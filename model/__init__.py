7c0f8e2bcc6b523170fedb1651985a29ecf030a6
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-07-20 15:21:59 +0200

    Remove CSTR model

--- model/__init__.py
+++ model/__init__.py
@@ -1,9 +1,8 @@
 from model.NonlinearModel import NonlinearModel
 from model.BioreactorModel import Bioreactor
-from model.CSTRModel import CSTRModel
 from model.LinearModel import LinearModel
 from model.TankModel import TankModel
 from model.DiagTank import DiagTank
 from model.LinkedTanks import LinkedTanks
 
-__all__ = ['Bioreactor', 'CSTRModel', 'LinearModel', 'TankModel', 'DiagTank', 'LinkedTanks']
+__all__ = ['Bioreactor', 'LinearModel', 'TankModel', 'DiagTank', 'LinkedTanks']
