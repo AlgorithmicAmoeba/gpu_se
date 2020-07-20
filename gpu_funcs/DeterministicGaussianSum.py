5b492d6f14407b98d62c62ba695517c36686be8c
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-07-20 11:25:58 +0200

    Add docstring

--- gpu_funcs/DeterministicGaussianSum.py
+++ gpu_funcs/DeterministicGaussianSum.py
@@ -5,32 +5,61 @@ import gpu_funcs.MultivariateGaussianSum
 
 class DeterministicGaussianSum(gpu_funcs.MultivariateGaussianSum):
     """Creates a MultivariateGaussianSum singleton that always returns the same values.
-    Useful for testing."""
+    Useful for testing.
 
-    values = numpy.array([], dtype=numpy.float32)
+    Parameters
+    ----------
+    means : library.array
+        A (N_distributions x Nx) array of the mean for each Gaussian
+
+    covariances : library.array
+        A (N_distributions x Nx x Nx) array of the covariance for each Gaussian
+
+    weights : library.array
+        A (N_distributions) array of the weighting for each Gaussian
+
+    library : {numpy, cupy}
+        The library to be used for array operations.
+        numpy is used for CPU implementations.
+        cupy is used for GPU implementations
+    """
+
+    __values = numpy.array([], dtype=numpy.float32)
 
     def __init__(self,  means, covariances, weights, library=cupy):
         super().__init__(means, covariances, weights, library)
 
     def draw(self, shape=(1, )):
+        """Draw samples from the distribution
+
+        Parameters
+        ----------
+        shape : {int, tuple} (optional)
+            Output shape
+
+        Returns
+        -------
+        out : library.array
+            A (*shape x Nx) array of samples
+        """
         if not isinstance(shape, tuple):
             shape = (shape,)
 
-        size = int(numpy.prod(shape)) * self.Nx
+        size = int(numpy.prod(shape)) * self._Nx
 
-        if DeterministicGaussianSum.values.size < size:
-            drawn_vals = super().draw(size - DeterministicGaussianSum.values.size)
+        if DeterministicGaussianSum.__values.size < size:
+            drawn_vals = super().draw(size - DeterministicGaussianSum.__values.size)
 
             if self.lib == cupy:
                 drawn_vals = drawn_vals.get()
 
-            DeterministicGaussianSum.values = numpy.hstack([
-                DeterministicGaussianSum.values,
+            DeterministicGaussianSum.__values = numpy.hstack([
+                DeterministicGaussianSum.__values,
                 drawn_vals.flatten()
             ])
 
-        out = DeterministicGaussianSum.values[:size]
-        out = out.reshape(shape + (self.Nx,)).squeeze()
+        out = DeterministicGaussianSum.__values[:size]
+        out = out.reshape(shape + (self._Nx,)).squeeze()
         if self.lib == cupy:
             out = cupy.asarray(out)
         return out
--- gpu_funcs/MultivariateGaussianSum.py
+++ gpu_funcs/MultivariateGaussianSum.py
@@ -5,7 +5,24 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 class MultivariateGaussianSum:
-    """Allows efficient pdf lookup of multivarite Gaussian sum pdf for CPU or GPU code"""
+    """Multivarite Gaussian distribution class for CPU and GPU implementations
+
+    Parameters
+    ----------
+    means : library.array
+        A (N_distributions x Nx) array of the mean for each Gaussian
+
+    covariances : library.array
+        A (N_distributions x Nx x Nx) array of the covariance for each Gaussian
+
+    weights : library.array
+        A (N_distributions) array of the weighting for each Gaussian
+
+    library : {numpy, cupy}
+        The library to be used for array operations.
+        numpy is used for CPU implementations.
+        cupy is used for GPU implementations
+    """
 
     def __init__(self, means, covariances, weights, library=cupy):
         self.lib = library
@@ -13,43 +30,68 @@ class MultivariateGaussianSum:
         self.weights = self.lib.asarray(weights, dtype=self.lib.float32)
         self.covariances = self.lib.asarray(covariances, dtype=self.lib.float32)
 
-        self.inverse_covariances = self.lib.asarray(numpy.linalg.inv(covariances))
-        self.Nd, self.Nx = means.shape
+        self._inverse_covariances = self.lib.asarray(numpy.linalg.inv(covariances))
+        self._Nd, self._Nx = means.shape
 
-        self.constants = (2 * self.lib.pi) ** (-self.Nx / 2) / self.lib.sqrt(
+        self._constants = (2 * self.lib.pi) ** (-self._Nx / 2) / self.lib.sqrt(
             self.lib.linalg.det(self.covariances))
 
     def pdf(self, x):
+        """Get the value of the probability density function evaluated at a point.
+
+        Parameters
+        ----------
+        x : library.array
+            A (m x Nx) array of points at which to evaluate the pdf
+
+        Returns
+        -------
+        result : library.array
+            A (m) array of pdf values
+
+        """
         x = self.lib.atleast_2d(x)
         es = x[:, None, :] - self.means[None, :, :]
 
         # The code below does: exp[i] = es[i].T @ self.inverse_covariances_device[i] @ es[i]
-        exp = es[:, :, None, :] @ self.inverse_covariances[None, :, :, :] @ es[:, :, :, None]
+        exp = es[:, :, None, :] @ self._inverse_covariances[None, :, :, :] @ es[:, :, :, None]
         r = self.lib.exp(-0.5*exp).squeeze()
 
         # The code below does: result = sum(r[i] * self.weights_device[i] * self.constants_device[i])
-        result = numpy.sum(self.lib.atleast_2d(self.constants * self.weights * r), axis=1)
+        result = numpy.sum(self.lib.atleast_2d(self._constants * self.weights * r), axis=1)
 
         return result
 
     def draw(self, shape=(1,)):
+        """Draw samples from the distribution
+
+        Parameters
+        ----------
+        shape : {int, tuple} (optional)
+            Output shape
+
+        Returns
+        -------
+        out : library.array
+            A (*shape x Nx) array of samples
+        """
         if not isinstance(shape, tuple):
             shape = (shape,)
 
         size = int(numpy.prod(shape))
         bins = self.lib.bincount(
             self.lib.random.choice(
-                self.lib.arange(self.Nd),
+                self.lib.arange(self._Nd),
                 size,
                 p=self.weights
             ),
-            minlength=self.Nd
+            minlength=self._Nd
         )
-        out = self.lib.empty((size, self.Nx), dtype=self.lib.float32)
+        out = self.lib.empty((size, self._Nx), dtype=self.lib.float32)
 
         index = 0
         for n, mean, cov in zip(bins, self.means, self.covariances):
             out[index:index + n] = self.lib.random.multivariate_normal(mean, cov, int(n))
             index += n
 
-        return out.reshape(shape + (self.Nx,)).squeeze()
+        return out.reshape(shape + (self._Nx,)).squeeze()
