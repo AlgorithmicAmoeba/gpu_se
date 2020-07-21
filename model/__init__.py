62d766d24df9fa4b990ca55a31037064a4a96ac5
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-07-21 07:37:06 +0200

    Remove moved files

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,5 @@
 from model.NonlinearModel import NonlinearModel
 from model.BioreactorModel import Bioreactor
 from model.LinearModel import LinearModel
-from tests.mpc_tests.TankModel import TankModel
-from tests.mpc_tests.DiagTank import DiagTank
 
-__all__ = ['Bioreactor', 'LinearModel', 'TankModel', 'DiagTank', 'LinkedTanks']
+__all__ = ['Bioreactor', 'LinearModel']
