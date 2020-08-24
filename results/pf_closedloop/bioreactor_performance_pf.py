06a5ee58fc9df644c34b0d5f7b930e9d6d3fe74c
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-08-24 09:16:31 +0200

    Add performance plots

--- results/pf_closedloop/bioreactor_performance_pf.py
+++ results/pf_closedloop/bioreactor_performance_pf.py
@@ -72,6 +72,41 @@ def performance_per_joule():
     return N_particles, ppjs, mpc_fracss
 
 
+def performance():
+    run_seqss = PF_run_seq.cpu_gpu_run_seqs()
+
+    performancess = []
+    for cpu_gpu in range(2):
+        dt_controls = numpy.min(run_seqss[cpu_gpu][0][1], axis=1)
+        dt_predicts = dt_controls.copy()
+        N_particles = run_seqss[cpu_gpu][0][0]
+        for method in range(1, 3):
+            _, run_seqs = run_seqss[cpu_gpu][method]
+            times = numpy.min(run_seqs, axis=1)
+            dt_controls += times
+
+        dt_controls = numpy.maximum(dt_controls, 1)
+        dt_predicts = numpy.maximum(dt_predicts, 0.1)
+
+        performances = []
+        for i in range(len(N_particles)):
+            sim = get_sim(
+                int(N_particles[i]),
+                dt_controls[i],
+                dt_predicts[i],
+                pf=True
+            )
+
+            performances.append(sim.performance)
+
+        performancess.append(performances)
+
+    N_particles = [run_seqss[0][0][0], run_seqss[1][0][0]]
+    performancess = numpy.array(performancess)
+
+    return N_particles, performancess
+
+
 def plot_ppjs():
     N_particles, ppjs, _ = performance_per_joule()
     plt.semilogy(numpy.log2(N_particles[0]), ppjs[0], 'k.', label='CPU')
