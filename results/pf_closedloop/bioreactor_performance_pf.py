765271456f3f127d2c8b5b58077a6732fc085d17
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-08-27 09:09:25 +0200

    Add covariance convergence plots

--- results/pf_closedloop/bioreactor_performance_pf.py
+++ results/pf_closedloop/bioreactor_performance_pf.py
@@ -1,6 +1,8 @@
 import numpy
 import sim_base
 import joblib
+import matplotlib
+import matplotlib.cm, matplotlib.colors
 import matplotlib.pyplot as plt
 import sys
 import os
@@ -8,6 +10,7 @@ sys.path.append(os.path.abspath('../pf_openloop'))
 # noinspection PyUnresolvedReferences
 import PF_run_seq
 import PF_power
+import decorators
 
 
 memory = joblib.Memory('cache/')
@@ -18,6 +21,17 @@ def get_sim(N_particles, dt_control, dt_predict, end_time=50, pf=True):
     sim = sim_base.Simulation(N_particles, dt_control, dt_predict, end_time, pf)
     sim.simulate()
     ans = sim.performance, sim.mpc_frac, sim.predict_count, sim.update_count
+    return ans
+
+
+memory = joblib.Memory('cache/covariance')
+
+
+@memory.cache
+def get_covariance(N_particles, dt_control, dt_predict, end_time=50, pf=True):
+    sim = sim_base.Simulation(N_particles, dt_control, dt_predict, end_time, pf)
+    sim.simulate()
+    ans = sim.covariance_point_size
     return ans
 
 
@@ -107,6 +121,43 @@ def get_performance():
     return N_particles, performancess
 
 
+@decorators.Pickler.pickle_me
+def covariance_point_estimates():
+    run_seqss = PF_run_seq.cpu_gpu_run_seqs()
+
+    pcovss = []
+    for cpu_gpu in range(2):
+        dt_controls = numpy.min(run_seqss[cpu_gpu][0][1], axis=1)
+        dt_predicts = dt_controls.copy()
+        N_particles = run_seqss[cpu_gpu][0][0]
+        for method in range(1, 3):
+            _, run_seqs = run_seqss[cpu_gpu][method]
+            times = numpy.min(run_seqs, axis=1)
+            dt_controls += times
+
+        dt_controls = numpy.maximum(dt_controls, 1)
+        dt_predicts = numpy.maximum(dt_predicts, 0.1)
+
+        pcovs = []
+        for i in range(len(N_particles)):
+            pcov = get_covariance(
+                int(N_particles[i]),
+                dt_controls[i],
+                dt_predicts[i],
+                pf=True,
+                end_time=500
+            )
+
+            pcovs.append(pcov)
+
+        pcovss.append(pcovs)
+
+    N_particles = [run_seqss[0][0][0], run_seqss[1][0][0]]
+    pcovss = numpy.array(pcovss)
+
+    return N_particles, pcovss
+
+
 def plot_ppjs():
     N_particles, ppjs, _ = performance_per_joule()
     plt.semilogy(numpy.log2(N_particles[0]), ppjs[0], 'k.', label='CPU')
@@ -144,6 +195,31 @@ def plot_performances():
     plt.show()
 
 
-plot_ppjs()
-plot_mpc_fracs()
-plot_performances()
+def plot_pcov():
+    N_particles, pcovss = covariance_point_estimates()
+    cmap = matplotlib.cm.get_cmap('plasma')
+    ts = numpy.linspace(0, 500, 5000)
+
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5), sharey='row')
+    for cpu_gpu in range(2):
+        ax = axes[cpu_gpu]
+        log2_Npart = numpy.log2(N_particles[cpu_gpu])
+        norm = matplotlib.colors.Normalize(vmin=log2_Npart[0], vmax=log2_Npart[-1])
+        for i in range(len(log2_Npart)):
+            ax.plot(ts, pcovss[cpu_gpu][i], color=cmap(norm(log2_Npart[i])))
+        ax.set_title(['CPU', 'GPU'][cpu_gpu])
+        ax.set_xlabel('Time (min)')
+
+        if cpu_gpu:
+            fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=cmap), ax=plt.gcf().gca())
+        else:
+            ax.set_ylabel(r'$\bar{\sigma}(\Sigma)$')
+
+    plt.savefig('PF_cov_con.pdf')
+    plt.show()
+
+
+# plot_ppjs()
+# plot_mpc_fracs()
+# plot_performances()
+plot_pcov()
