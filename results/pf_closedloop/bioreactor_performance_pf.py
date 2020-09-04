a26fd94c41cdb86f014c6056ae19ff700011975f
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-09-04 09:56:47 +0200

    Condense common code

--- results/pf_closedloop/bioreactor_performance_pf.py
+++ results/pf_closedloop/bioreactor_performance_pf.py
@@ -1,11 +1,13 @@
-import numpy
-import sim_base
+import os
+import sys
 import joblib
 import matplotlib
-import matplotlib.cm, matplotlib.colors
+import matplotlib.cm
+import matplotlib.colors
 import matplotlib.pyplot as plt
-import sys
-import os
+import numpy
+import sim_base
+
 sys.path.append(os.path.abspath('../pf_openloop'))
 # noinspection PyUnresolvedReferences
 import PF_run_seq
@@ -20,30 +22,20 @@ memory = joblib.Memory('cache/')
 def get_sim(N_particles, dt_control, dt_predict, end_time=50, pf=True):
     sim = sim_base.Simulation(N_particles, dt_control, dt_predict, end_time, pf)
     sim.simulate()
-    ans = sim.performance, sim.mpc_frac, sim.predict_count, sim.update_count
+    ans = sim.performance, sim.mpc_frac, sim.predict_count, sim.update_count, sim.covariance_point_size
     return ans
 
 
-memory = joblib.Memory('cache/covariance')
-
-
-@memory.cache
-def get_covariance(N_particles, dt_control, dt_predict, end_time=50, pf=True):
-    sim = sim_base.Simulation(N_particles, dt_control, dt_predict, end_time, pf)
-    sim.simulate()
-    ans = sim.covariance_point_size
-    return ans
-
-
-def performance_per_joule():
+@decorators.Pickler.pickle_me
+def get_results():
     run_seqss = PF_run_seq.cpu_gpu_run_seqs()
     powerss = PF_power.cpu_gpu_power_seqs()
 
-    ppjs, mpc_fracss = [], []
+    ppjs, mpc_fracss, performancess, pcovss = [], [], [], []
     for cpu_gpu in range(2):
         dt_controls = numpy.min(run_seqss[cpu_gpu][0][1], axis=1)
         dt_predicts = dt_controls.copy()
-        N_particles = run_seqss[cpu_gpu][0][0]
+        N_particles = run_seqss[0][0][0]
         for method in range(1, 3):
             _, run_seqs = run_seqss[cpu_gpu][method]
             times = numpy.min(run_seqs, axis=1)
@@ -61,9 +53,9 @@ def performance_per_joule():
 
             method_power.append(power)
 
-        ppj, mpc_fracs = [], []
+        ppj, mpc_fracs, performances, pcovs = [], [], [], []
         for i in range(len(N_particles)):
-            performance, mpc_frac, predict_count, update_count = get_sim(
+            performance, mpc_frac, predict_count, update_count, pcov = get_sim(
                 int(N_particles[i]),
                 dt_controls[i],
                 dt_predicts[i],
@@ -75,91 +67,25 @@ def performance_per_joule():
             total_power = predict_count * predict_power + update_count * (update_power + resample_power)
             ppj.append(performance / total_power)
             mpc_fracs.append(mpc_frac)
+            performances.append(performance)
+            pcovs.append(pcov)
 
         ppjs.append(ppj)
         mpc_fracss.append(mpc_fracs)
-
-    N_particles = [run_seqss[0][0][0], run_seqss[1][0][0]]
-    ppjs = numpy.array(ppjs)
-    mpc_fracss = numpy.array(mpc_fracss)
-
-    return N_particles, ppjs, mpc_fracss
-
-
-def get_performance():
-    run_seqss = PF_run_seq.cpu_gpu_run_seqs()
-
-    performancess = []
-    for cpu_gpu in range(2):
-        dt_controls = numpy.min(run_seqss[cpu_gpu][0][1], axis=1)
-        dt_predicts = dt_controls.copy()
-        N_particles = run_seqss[cpu_gpu][0][0]
-        for method in range(1, 3):
-            _, run_seqs = run_seqss[cpu_gpu][method]
-            times = numpy.min(run_seqs, axis=1)
-            dt_controls += times
-
-        dt_controls = numpy.maximum(dt_controls, 1)
-        dt_predicts = numpy.maximum(dt_predicts, 0.1)
-
-        performances = []
-        for i in range(len(N_particles)):
-            performance, _, _, _ = get_sim(
-                int(N_particles[i]),
-                dt_controls[i],
-                dt_predicts[i],
-                pf=True
-            )
-
-            performances.append(performance)
-
         performancess.append(performances)
-
-    N_particles = [run_seqss[0][0][0], run_seqss[1][0][0]]
-    performancess = numpy.array(performancess)
-
-    return N_particles, performancess
-
-
-@decorators.Pickler.pickle_me
-def covariance_point_estimates():
-    run_seqss = PF_run_seq.cpu_gpu_run_seqs()
-
-    pcovss = []
-    for cpu_gpu in range(2):
-        dt_controls = numpy.min(run_seqss[cpu_gpu][0][1], axis=1)
-        dt_predicts = dt_controls.copy()
-        N_particles = run_seqss[cpu_gpu][0][0]
-        for method in range(1, 3):
-            _, run_seqs = run_seqss[cpu_gpu][method]
-            times = numpy.min(run_seqs, axis=1)
-            dt_controls += times
-
-        dt_controls = numpy.maximum(dt_controls, 1)
-        dt_predicts = numpy.maximum(dt_predicts, 0.1)
-
-        pcovs = []
-        for i in range(len(N_particles)):
-            pcov = get_covariance(
-                int(N_particles[i]),
-                dt_controls[i],
-                dt_predicts[i],
-                pf=True,
-                end_time=500
-            )
-
-            pcovs.append(pcov)
-
         pcovss.append(pcovs)
 
-    N_particles = [run_seqss[0][0][0], run_seqss[1][0][0]]
+    N_particles = [run_seqss[0][0][0], run_seqss[0][0][0]]
+    ppjs = numpy.array(ppjs)
+    mpc_fracss = numpy.array(mpc_fracss)
+    performancess = numpy.array(performancess)
     pcovss = numpy.array(pcovss)
 
-    return N_particles, pcovss
+    return N_particles, ppjs, mpc_fracss, performancess, pcovss
 
 
 def plot_ppjs():
-    N_particles, ppjs, _ = performance_per_joule()
+    N_particles, ppjs, _, _, _ = get_results()
     plt.semilogy(numpy.log2(N_particles[0]), ppjs[0], 'k.', label='CPU')
     plt.semilogy(numpy.log2(N_particles[1]), ppjs[1], 'kx', label='GPU')
     plt.xlabel('$ \log_2(N) $ particles')
@@ -171,7 +97,7 @@ def plot_ppjs():
 
 
 def plot_mpc_fracs():
-    N_particles, _, mpc_fracss = performance_per_joule()
+    N_particles, _, mpc_fracss, _, _ = get_results()
     plt.plot(numpy.log2(N_particles[0]), mpc_fracss[0], 'k.', label='CPU')
     plt.plot(numpy.log2(N_particles[1]), mpc_fracss[1], 'kx', label='GPU')
     plt.xlabel('$ \log_2(N) $ particles')
@@ -184,7 +110,7 @@ def plot_mpc_fracs():
 
 
 def plot_performances():
-    N_particles, performancess = get_performance()
+    N_particles, _, _, performancess, _ = get_results()
     plt.plot(numpy.log2(N_particles[0]), performancess[0], 'k.', label='CPU')
     plt.plot(numpy.log2(N_particles[1]), performancess[1], 'kx', label='GPU')
     plt.xlabel('$ \log_2(N) $ particles')
@@ -196,7 +122,7 @@ def plot_performances():
 
 
 def plot_pcov():
-    N_particles, pcovss = covariance_point_estimates()
+    N_particles, _, _, _, pcovss = get_results()
     cmap = matplotlib.cm.get_cmap('plasma')
     ts = numpy.linspace(0, 500, 5000)
 
@@ -219,7 +145,7 @@ def plot_pcov():
     plt.show()
 
 
-# plot_ppjs()
-# plot_mpc_fracs()
-# plot_performances()
+plot_ppjs()
+plot_mpc_fracs()
+plot_performances()
 plot_pcov()
