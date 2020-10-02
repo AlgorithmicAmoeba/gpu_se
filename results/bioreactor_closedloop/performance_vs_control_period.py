7225e310b544694bc18b4f801fc9fd996981f05f
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2020-10-02 09:47:05 +0200

    use picklejar for cache

--- results/bioreactor_closedloop/performance_vs_control_period.py
+++ results/bioreactor_closedloop/performance_vs_control_period.py
@@ -1,14 +1,16 @@
 import numpy
 import tqdm
 import matplotlib.pyplot as plt
-import pandas
 import sim_base
 import matplotlib
+from decorators import PickleJar
 
 
-def get_simulation_performance(dt_control):
+@PickleJar.pickle(path='bioreactor/perf_vs_cp/raw')
+def get_simulation_performance(dt_control, monte_carlo):
+    _ = monte_carlo
     end_time = 50
-    ts = numpy.linspace(0, end_time, end_time*10)
+    ts = numpy.linspace(0, end_time, end_time*20)
     dt = ts[1]
     assert dt <= dt_control
 
@@ -57,40 +59,27 @@ def get_simulation_performance(dt_control):
     return sim_base.performance(ys[:, lin_model.outputs], lin_model.yd2n(K.ysp), ts)
 
 
-def generate_results(redo=False, number=50, low=0.1, high=30.):
-    try:
-        if redo:
-            raise FileNotFoundError
+@PickleJar.pickle(path='bioreactor/perf_vs_cp/processed')
+def generate_results():
+    monte_carlos = 5
+    dt_controls, performances = [], []
+    for dt_control in tqdm.tqdm(numpy.linspace(0.1, 30, 20)):
+        for monte_carlo in range(monte_carlos):
+            y = get_simulation_performance(dt_control, monte_carlo)
+            if y > 1e8:
+                continue
+            dt_controls.append(dt_control)
+            performances.append(y)
 
-        df = pandas.read_csv('bioreactor_noisy_performance.csv', index_col=0)
-    except FileNotFoundError:
-        df = pandas.DataFrame(columns=['dt_controls', 'performance'])
-
-    dt_controls = numpy.round(
-        numpy.random.uniform(
-            low=low,
-            high=high,
-            size=number
-        ),
-        1
-    )
-    #
-    performances = numpy.array([
-        get_simulation_performance(dt_control) for dt_control in tqdm.tqdm(dt_controls)
-    ])
-
-    res = numpy.vstack([dt_controls, performances])
-    df_new = pandas.DataFrame(res.T, columns=['dt_controls', 'performance'])
-    df = df.append(df_new).sort_values(by='dt_controls')
-    df.to_csv('bioreactor_noisy_performance.csv')
+    return dt_controls, performances
 
 
 def plot_results():
     matplotlib.rcParams.update({'font.size': 9})
     plt.figure(figsize=(6.25/1.4, 5/1.4))
 
-    df = pandas.read_csv('bioreactor_noisy_performance.csv')
-    plt.plot(df['dt_controls'], df['performance'], 'k.')
+    dt_controls, performances = generate_results()
+    plt.plot(dt_controls, performances, 'k.')
     plt.axvline(0.1, color='red')
 
     # plt.title("Closedloop performance vs control period")
@@ -98,7 +87,7 @@ def plot_results():
     plt.xlabel('Control period (min)')
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
     # plt.axvline(1)
-    plt.savefig('noisy_performance.pdf')
+    plt.savefig('performance_vs_control_period.pdf')
     plt.show()
 
 
@@ -118,17 +107,16 @@ def plot_pretty_results():
     plt.gcf().set_facecolor(black)
     plt.gca().set_facecolor(black)
 
-    df = pandas.read_csv('bioreactor_noisy_performance.csv')
-    plt.plot(df['dt_controls'], df['performance'], '.', color=yellow)
+    dt_controls, performances = generate_results()
+    plt.plot(dt_controls, performances, '.', color=yellow)
     # plt.title("Closedloop performance vs control period")
     plt.ylabel(r'$P_{\mathrm{ITAE}}$')
     plt.xlabel('Control period (min)')
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])
-    plt.savefig('noisy_performance_pretty.png', transparent=True)
+    plt.savefig('performance_vs_control_period.png', transparent=True)
     plt.show()
 
 
 if __name__ == '__main__':
-    # generate_results(redo=False, number=100)
     plot_results()
     # plot_pretty_results()
